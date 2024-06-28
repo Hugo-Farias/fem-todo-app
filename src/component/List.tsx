72cf40061ff0e689d7dc40e665e25efc6ef9dea6
Hugo-Farias <hg80rod@gmail.com>
2024-06-28 11:43:50 -0300

    better implementation of check and remove, less code, cleaner

--- src/component/List.tsx
+++ src/component/List.tsx
@@ -3,7 +3,6 @@ import checkbox from "../assets/icon-checkbox.svg";
 import checkmarked from "../assets/icon-checkmarked.svg";
 import { dataType } from "../App.tsx";
 import { AnimatePresence, motion } from "framer-motion";
-import React from "react";
 
 type propT = {
   data: dataType;
@@ -12,19 +11,6 @@ type propT = {
 };
 
 function List({ data, remove, mark }: propT) {
-  const handleCheck = function (e: React.MouseEvent<HTMLElement>) {
-    const target = e.target as HTMLDivElement;
-    const id = target.closest("li")!.getAttribute("data-id");
-
-    if (!id) return;
-
-    mark(+id);
-  };
-
-  const handleRemove = function (id: number) {
-    remove(id);
-  };
-
   return (
     <motion.ul className="w-full overflow-hidden rounded-xl text-content drop-shadow-xl">
       <AnimatePresence>
@@ -32,10 +18,10 @@ function List({ data, remove, mark }: propT) {
           <motion.li
             layout
             transition={{ duration: 0.2 }}
-            exit={{ scale: 0.9, x: "10%", opacity: 0 }}
+            exit={{ scale: 0.9, x: "30%", opacity: 0 }}
             key={v.id}
             data-id={v.id}
-            onClick={handleCheck}
+            onClick={() => mark(v.id)}
             className="group flex h-12 items-center gap-3 border-b-[1px] border-content/20 bg-bkg transition-colors duration-200 hover:cursor-pointer"
           >
             <img
@@ -50,7 +36,10 @@ function List({ data, remove, mark }: propT) {
             </div>
             <button
               className="h-full w-12 hover:bg-black/10"
-              onClick={() => handleRemove(v.id)}
+              onClick={(e) => {
+                e.stopPropagation();
+                remove(v.id);
+              }}
             >
               <img
                 className="mx-auto size-3"
